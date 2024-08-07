65eb7377b2c25d24ba87d3bcb53508d2c3333ef4
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-08-06 17:11:28 -0700

    add an example for if

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// We want to make a fortune telling machine
+// takes your name as input
+// and outputs your fortune
+
+// Name is longer than 7 characters: "will marry late in life"
+// Name is shorter than 5 characters: "will marry within a year."
+// name is between 5 and 7 characters (inclusive): "will encounter a once-in-a-lifetime opurtunity"
+// first letter is 'R': "will be rich"
+// Name contains an 'i': "will fall in love this week"
+
+let myName;
+myName = "Bob";
+// get the length of the name
+let lengthOfName = myName.length;
+// if the name is shorter than 5 give marry early fortune
+if (lengthOfName < 5) {
+    console.log("You will marry soon!");
+}
